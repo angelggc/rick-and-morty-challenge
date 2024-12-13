220cd24ad1caf9d558ffe3546174ba90c1f3a7f8
angelggc <angelgimenez992@gmail.com>
2024-12-13 10:01:19 -0300

    integrate api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Character {
+  id: number;
+  gender: string;
+  created: string;
+  image: string;
+  name: string;
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+  location: {
+    name: string;
+    url: string;
+  };
+  origin: {
+    name: string;
+    url: string;
+  };
+}
+
 function App() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  async function getData() {
+    await fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((e) => console.log(e));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <h1>Hola mundo</h1>
+      {characters.map((c) => (
+        <p>{c.name}</p>
+      ))}
     </>
   );
 }
