0bed42cf0b8a545e3a778e6d8eab1726602ba202
angelggc <angelgimenez992@gmail.com>
2024-12-13 10:49:53 -0300

    add change orderselect and modal

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,46 @@ interface Character {
   };
 }
 
+interface ModalProps {
+  onClose: () => void;
+  character: Character;
+}
+
+function Modal({ onClose, character }: ModalProps) {
+  return (
+    <div
+      id="modal"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <div className="bg-white rounded-lg p-6">
+        <p>Nombre: {character.name}</p>
+        <button
+          onClick={onClose}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState<Character>();
+
   async function getData() {
     await fetch("https://rickandmortyapi.com/api/character")
       .then((response) => response.json())
-      .then((data) => setCharacters(data.results))
+      .then((data) =>
+        setCharacters(
+          data.results.sort((a: Character, b: Character) =>
+            a.name.localeCompare(b.name)
+          )
+        )
+      )
       .catch((e) => console.log(e));
   }
 
@@ -33,13 +67,44 @@ function App() {
     getData();
   }, []);
 
+  useEffect(() => {
+    console.log(characters);
+  }, [characters]);
+
   return (
     <>
       <div className="flex justify-center">
         <div className="container">
+          <header className="h-24 bg-slate-900">
+            <select
+              name=""
+              id=""
+              onChange={(e) => {
+                if (e.target.value === "A-Z") {
+                  setCharacters(
+                    characters.sort((a, b) => a.name.localeCompare(b.name))
+                  );
+                }
+                if (e.target.value === "Z-A") {
+                  setCharacters(
+                    characters.sort((a, b) => b.name.localeCompare(a.name))
+                  );
+                }
+              }}
+            >
+              <option value="A-Z">A - Z</option>
+              <option value="Z-A">Z - A</option>
+            </select>
+          </header>
           <div className="flex flex-wrap justify-center gap-4">
             {characters.map((c) => (
-              <div className="border flex flex-col items-center bg-green-600 w-48 p-3">
+              <div
+                onClick={() => {
+                  setShowModal(true);
+                  setSelectedCharacter(c);
+                }}
+                className="border flex flex-col items-center bg-green-600 w-48 p-3"
+              >
                 <img src={c.image} alt="" className="w-44" />
                 <div className="text-2xl items-center flex flex-col">
                   <p className="text-center">{c.name}</p>
@@ -48,6 +113,15 @@ function App() {
               </div>
             ))}
           </div>
+          {showModal && selectedCharacter && (
+            <Modal
+              onClose={() => {
+                setShowModal(false);
+                setSelectedCharacter(undefined);
+              }}
+              character={selectedCharacter}
+            />
+          )}
         </div>
       </div>
     </>
